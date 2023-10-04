ec27cac91cdc6cd85a58ce617d30bc5aa05eeb9a
Mamun Miah <mamunmiah193gsc@gmail.com>
2023-10-04 23:12:54 +0600

    Some input output added

--- Lab_Animal_Management_System/Main.java
+++ Lab_Animal_Management_System/Main.java
@@ -1,12 +1,18 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         Lab lab = new Lab();
 
         lab.addCage(new Cage("Cage001"));
-
         LabManager labManager = new LabManager(lab);
         labManager.addAnimalToCage("Cage001", new Mammal("A001", "Ginuepig", "Female", 6, "black"));
+        labManager.addAnimalToCage("Cage001", new Mammal("A002", "Tiger", "Male", 6, "Gray"));
+        labManager.addAnimalToCage("Cage001", new Birds("A003", "Pigion", "Male", 5, true));
         System.out.println(labManager.findAnimalById("A001"));
+        System.out.println(labManager.findAnimalById("A002"));
+        labManager.removeAnimalFromCage("Cage001", "A001");
+        System.out.println(labManager.findAnimalById("A002"));
     }
     
 }
